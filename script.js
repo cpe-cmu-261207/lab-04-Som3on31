d5a7e4ab7f6744eaac33536087c83788f94211cc
Som3on31 <saharit.f4@gmail.com>
2021-07-25 10:14:03 +0700

    reworked

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 /* Your code here */
 
-function addList(){
-    alert("List cannot be empty.")
+let currentInput = ''
+let list = {
+    toDo: [],
+    finished: []
 }
+
+const input = document.createElement('input')
+input.innerHTML = 'Add your todo here'
+
+input.addEventListener('input', event => {
+    currentInput = event.target.value
+})
+input.addEventListener('keyup', event => {
+    if (event.keyCode === 13 && currentInput === '') alert ('List cannot be empty.')
+})
+
+const button = document.createElement('button')
+button.innerHTML = '+'
+
+input.addEventListener('click', event =>{
+    if (currentInput === '') alert ('List cannot be empty.')
+    else{
+        
+    }
+})
+
+document.body.append(input)
+document.body.append(button)
