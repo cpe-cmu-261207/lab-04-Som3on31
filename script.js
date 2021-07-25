22a16dbb121cece26366adadf42a18ecd1cba2e1
Som3on31 <saharit.f4@gmail.com>
2021-07-25 17:12:40 +0700

    tweaked alignment

--- script.js
+++ script.js
@@ -6,13 +6,38 @@ let list = {
     finished: []
 }
 
-const runner = document.getElementById('tester')
+const input = document.createElement('input')
+input.placeholder = 'Add your todo list here'
 
+input.addEventListener('input', event => {
+    currentInput = event.target.value
+})
+input.addEventListener('keyup', event => {
+    if (event.keyCode === 13 && currentInput === '') alert('Task cannot be empty.')
+    if (event.keyCode === 13) {
+        addTask();
+    }
+})
+
+const mainDiv = document.createElement('div')
+
+const button = document.createElement('button')
+button.innerHTML = '+'
+
+button.addEventListener('click', event => {
+    if (currentInput === '') alert('Task cannot be empty.')
+    else {
+        addTask();
+    }
+})
+
+const runner = document.getElementById('tester')
+runner.append(input)
+runner.append(button)
 
 function addTask() {
-    runner.append(input)
-    runner.append(button)
-    
+
+
     const divTask = document.createElement('div')
 
     divTask.addEventListener('mouseenter', () => {
@@ -51,31 +76,4 @@ function addTask() {
 }
 
 
-const input = document.createElement('input')
-input.placeholder = 'Add your todo list here'
-
-input.addEventListener('input', event => {
-    currentInput = event.target.value
-})
-input.addEventListener('keyup', event => {
-    if (event.keyCode === 13 && currentInput === '') alert('Task cannot be empty.')
-    if (event.keyCode === 13) {
-        addTask();
-    }
-})
-
-const mainDiv = document.createElement('div')
-
-const button = document.createElement('button')
-button.innerHTML = '+'
-
-button.addEventListener('click', event => {
-    if (currentInput === '') alert('Task cannot be empty.')
-    else {
-        addTask();
-    }
-})
-
-document.body.append(input)
-document.body.append(button)
 document.body.append(mainDiv)
